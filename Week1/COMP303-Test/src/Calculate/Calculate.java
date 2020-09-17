0a5e0142b729594a4d4b3278a23f852aa2fb61e2
Jing <xjxu0629@gmail.com>
2020-09-17 11:08:58 -0400

    Added Prac1 (COMP303-Test)

---
+++ Week1/COMP303-Test/src/Calculate/Calculate.java
@@ -0,0 +1,18 @@
+package Calculate;
+
+
+public class Calculate {
+
+	static int output=1;
+	
+	public static void main(String[] args) {
+		
+		for(int i=0; i<5;i++) {
+			output=output+i;			
+		}
+		
+		System.out.println("The result is " + output);
+
+	}
+
+}
